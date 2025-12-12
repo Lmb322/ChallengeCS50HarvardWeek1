4f00f0de918315b752e66d5c49fa00d7757cdac9
Lucas Belgoni <lucas.belgoni@gmail.com>
2025-12-11 22:33:53 -0300

    update

--- challenge_mario_less.py
+++ challenge_mario_less.py
@@ -2,5 +2,5 @@ altura = int(input('Digite a altura: '))
 bloco = '#'
 espaco = ' ' 
 for i in range (altura):
-    qtde_espaco = altura - i 
+    qtde_espaco = altura - i - 1
     print(f'{espaco*qtde_espaco}{bloco*(i+1)} {bloco*(i+1)}')
