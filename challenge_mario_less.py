05751843854f82d3c71a5c9405d743966833d235
Lmb22 <lucas.belgoni@gmail.com>
2025-12-11 23:16:16 -0300

    last commit

--- challenge_mario_less.py
+++ challenge_mario_less.py
@@ -1,6 +1,15 @@
-altura = int(input('Digite a altura: '))
 bloco = '#'
-espaco = ' ' 
-for i in range (altura):
-    qtde_espaco = altura - i - 1
-    print(f'{espaco*qtde_espaco}{bloco*(i+1)}')
+espaco = ' '
+while True:
+    try:
+        altura = int(input('Digite a altura de 1 a 8: '))
+        if altura > 0 and altura < 9:
+            for i in range (altura):
+                qtde_espaco = altura - i - 1
+                print(f'{espaco*qtde_espaco}{bloco*(i+1)}')
+            break
+        else:
+            print('Numero invalido')
+    except ValueError:
+        print('Erro no caractere inserido digite apenas números inteiros!')
+    continue
--- challenge_mario_more.py
+++ challenge_mario_more.py
@@ -1,10 +1,15 @@
-altura = int(input('Digite a altura: '))
 bloco = '#'
-espaco = ' ' 
-for i in range (altura):
-    qtde_espaco = altura - i - 1
-<<<<<<< HEAD:challenge_mario_more.py
-    print(f'{espaco*qtde_espaco}{bloco*(i+1)} {bloco*(i+1)}')
-=======
-    print(f'{espaco*qtde_espaco}{bloco*(i+1)} {bloco*(i+1)}')
->>>>>>> 4f00f0de918315b752e66d5c49fa00d7757cdac9:challenge_mario_less.py
+espaco = ' '
+while True:
+    try:
+        altura = int(input('Digite a altura de 1 a 8: '))
+        if altura > 0 and altura < 9:
+            for i in range (altura):
+                qtde_espaco = altura - i - 1
+                print(f'{espaco*qtde_espaco}{bloco*(i+1)} {bloco*(i+1)}')
+            break
+        else:
+            print('Numero invalido')
+    except ValueError:
+        print('Erro digite apenas numeros inteiros.')
+    continue
